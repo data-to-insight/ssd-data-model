9c2441944ec44b0d4799db8dac03dd6a7f3a384f
Kaj Siebert <kaj@k-si.com>
2021-07-13 11:48:31 +0100

    Turn of sort_keys

--- rtofdata/yaml.py
+++ rtofdata/yaml.py
@@ -2,4 +2,4 @@ import yaml
 
 def write_yaml(data: dict, filename: str):
     with open(filename, 'wt') as file:
-        yaml.dump(data, file)
+        yaml.dump(data, file, sort_keys=False, allow_unicode=True)
