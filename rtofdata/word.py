9799b2e14f0215450e0231bba12cb3a69445f715
JamesHearn2 <james.hearn@socialfinance.org.uk>
2021-07-19 14:29:44 +0100

    initial updates docx word output

--- rtofdata/word.py
+++ rtofdata/word.py
@@ -1,4 +1,6 @@
 from docx import Document
+from docx.shared import Cm, Inches
+from docx.enum.table import WD_TABLE_ALIGNMENT
 
 
 
@@ -10,8 +12,8 @@ def write_word_specification(data: dict, filename: str):
     """
     document = Document()
     document.add_heading('RTOF Data Specification', 0)
-
-    document.add_paragraph('Introduction Here')
+    introduction = "The document outlines the data fields, types and descriptions for all required data collection during the lifetime of the Refugee Tranisition Outcome Fund"
+    document.add_paragraph(introduction)
     document.add_page_break()
 
     document.add_heading('Tables', 1)
@@ -22,11 +24,23 @@ def write_word_specification(data: dict, filename: str):
     )
     for table_def in data['Tables']:
         table_name = table_def.get('Table')
-        document.add_heading(table_name, 2)
+        document.add_heading(table_name.title(), 2)
         document.add_paragraph(table_def.get('Description', ""))
         
         table = document.add_table(rows=1, cols=4)
-        table.style = 'TableGrid'
+        table.style = 'Light Grid Accent 5'
+        table.allow_autofit = False
+        table.autofit = False
+        table.alignment = WD_TABLE_ALIGNMENT.CENTER
+
+        col0 = table.columns[0]
+        col1 = table.columns[1]
+        col2 = table.columns[2]
+        col3 = table.columns[3]
+        col0.width = Cm(5.19)        
+        col1.width = Cm(2.5)
+        col2.width = Cm(2.5)
+        col3.width = Cm(6.0)
         
         hdr_cells = table.rows[0].cells
         hdr_cells[0].text = 'ID'
@@ -42,5 +56,6 @@ def write_word_specification(data: dict, filename: str):
             row_cells[1].text = f.get('Name', "")
             row_cells[2].text = f.get('Type', "")
             row_cells[3].text = f.get('Description', "")
-            
+        
+
     document.save(filename)
