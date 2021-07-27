b6cd7c655f8bfa197ffb598097576619f69af5e5
Kaj Siebert <kaj@k-si.com>
2021-07-27 12:07:20 +0100

    Add docx template

--- rtofdata/word.py
+++ rtofdata/word.py
@@ -1,11 +1,14 @@
+from datetime import datetime
 from pathlib import Path
 
-from docx import Document
-from docx.shared import Cm, Inches
-from docx.enum.table import WD_TABLE_ALIGNMENT
-from docx.enum.text import WD_ALIGN_PARAGRAPH
+from docx.shared import Cm
+from docxtpl import DocxTemplate, InlineImage
 
+import git
 
+root = Path(__file__).parent / '..'
+input_dir = root / 'data'
+output_dir = root / 'output'
 
 
 def write_word_specification(data: dict, filename: str):
@@ -13,61 +16,37 @@ def write_word_specification(data: dict, filename: str):
     Creates a detailed Word version of the specification.
     Read more about docx here: https://python-docx.readthedocs.io/en/latest/
     """
-        
-    document = Document()
-    document.add_heading('RTOF Data Specification', 0)
-    for table_def in data['Intro_table']:
-        introduction = table_def.get('Introduction')
-        document.add_paragraph(introduction)
 
-    picture = Path(__file__).parent / '../data/RTOF_program_path.png'
-    document.add_picture(str(picture.absolute()), width = Cm(18.0))
-    last_paragraph = document.paragraphs[-1] 
-    last_paragraph.alignment = WD_ALIGN_PARAGRAPH.CENTER
-    
-    document.add_page_break()
+    tpl = DocxTemplate(input_dir / "template.docx")
 
-    document.add_heading('Forms', 1)
-    document.add_paragraph(
-        'Each from corresponds to a stage '
-        'in the service.'
-    )
-    for count, table_def in enumerate(data['Tables']):
-        table_name = table_def.get('Table')
-        if not table_name:
-            continue
-        document.add_heading(f"Form {count+1}: {table_name.title()}", 2)
-        document.add_paragraph(table_def.get('Description', ""))
-        
-        table = document.add_table(rows=1, cols=4)
-        table.style = 'Light Grid Accent 5'
-        table.allow_autofit = False
-        table.autofit = False
-        table.alignment = WD_TABLE_ALIGNMENT.CENTER
+    repo = git.Repo(search_parent_directories=True)
+    git_version = repo.head.object.hexsha[:7]
 
-        col0 = table.columns[0]
-        col1 = table.columns[1]
-        col2 = table.columns[2]
-        col3 = table.columns[3]
-        col0.width = Cm(5.2)        
-        col1.width = Cm(2.5)
-        col2.width = Cm(2.5)
-        col3.width = Cm(6.0)
-        
-        hdr_cells = table.rows[0].cells
-        hdr_cells[0].text = 'ID'
-        hdr_cells[1].text = 'Name'
-        hdr_cells[2].text = 'Type'
-        hdr_cells[3].text = 'Description'
+    tagmap = {}
+    for t in repo.tags:
+        tagmap.setdefault(repo.commit(t), []).append(t)
 
-        table_fields = [f for f in data['Fields']
-                        if f.get('Table') == table_name]
-        for f in table_fields:
-            row_cells = table.add_row().cells
-            row_cells[0].text = f.get('ID', "")
-            row_cells[1].text = f.get('Name', "")
-            row_cells[2].text = f.get('Type', "")
-            row_cells[3].text = f.get('Description', "")
-        
+    git_tag = tagmap.get(repo.head.object)
+    if git_tag:
+        git_version = str(git_tag[0])
 
-    document.save(filename)
+    if repo.is_dirty() or len(repo.untracked_files) > 0:
+        git_version = f"{git_version} (changes pending)"
+
+    context = dict(git_version=git_version, generation_time=f"{datetime.now():%-d %B %Y}")
+
+    context['milestones_image'] = InlineImage(tpl, image_descriptor=str(input_dir / 'RTOF_program_path.png'),
+                                              width=Cm(16))
+
+    context['record_list'] = record_list = [{**t} for t in data['Tables']]
+    for record in record_list:
+        record['fields'] = [f for f in data['Fields'] if f['Table'] == record['Table']]
+
+    context['field_list'] = field_list = [{**t} for t in data['Fields']]
+    for field in field_list:
+        field['table'] = [t for t in data['Tables'] if t['Table'] == field['Table']]
+
+    field_list.sort(key=lambda f: f"{f['Table']}.{f['ID']}")
+
+    tpl.render(context)
+    tpl.save(output_dir / "specification.docx")
