93e7c624450e6e7e569d5a8fa7f400c991067d3a
JamesHearn2 <james.hearn@socialfinance.org.uk>
2021-07-30 17:45:22 +0100

    fake person data generator added

--- rtofdata/fake.py
+++ rtofdata/fake.py
@@ -1,5 +1,22 @@
 from faker import Faker
+import pandas as pd
+from datetime import datetime
+import numpy as np
 fake = Faker()
 
-j = fake.text()
-print(j)
+def create_data(x):
+    person_data = {}
+    genders = ["Male", "Female", "Non-binary", "Transgender", "Other"]
+    gender_weights = [0.2, 0.2, 0.2, 0.2, 0.2]
+    for i in range(0, x):
+        person_data[i] = {}
+        person_data[i]['NiNo'] = fake.bothify('?? ######')
+        person_data[i]['date_of_birth'] = fake.date_between(start_date = '-65y', end_date = ('-18y'))
+        person_data[i]['gender'] = np.random.choice(genders, p=gender_weights)
+        person_data[i]['dispersal_area'] = fake.postcode()
+        person_data[i]['date_started_service'] = fake.date_between(start_date = '-1y')
+    return person_data
+
+test_fake = create_data(10)
+print (pd.DataFrame.from_dict(test_fake)  )  
+
