33c28f407f97e76bff53c5406a95ea422854e987
JamesHearn2 <james.hearn@socialfinance.org.uk>
2021-08-04 15:39:25 +0100

    update to image in word doc

--- rtofdata/word.py
+++ rtofdata/word.py
@@ -55,10 +55,10 @@ def write_word_specification(spec: Specification):
     context = create_context(spec)
     context['milestones_image'] = InlineImage(tpl, image_descriptor=str(assets_dir / 'submission_and_collection.png'),
                                               width=Cm(16))
-
-    context = create_context(spec)
     context['milestones_image2'] = InlineImage(tpl, image_descriptor=str(assets_dir / 'Data_forms_to_be_submitted_2.png'),
                                               width=Cm(16))
+    
+    
 
     tpl.render(context)
     tpl.save(output_dir / "specification.docx")
