e373243ff11173d64737bed974b7e79b02080ee1
Rob Harrison <48765695+robjharrison@users.noreply.github.com>
2023-06-14 21:34:12 +0000

    display the returns group list on erd

--- create_erd_multiyaml.py
+++ create_erd_multiyaml.py
@@ -1,3 +1,32 @@
+# import glob
+# import yaml
+# import pygraphviz as pgv
+
+
+# # Create a new directed graph
+# G = pgv.AGraph(directed=True)
+
+# # Load and combine YAML files
+# for file_path in glob.glob('structure_split_objects/*.yml'):
+#     with open(file_path) as f:
+#         data = yaml.safe_load(f)
+#         nodes = data.get('nodes', [])
+#         relations = data.get('relations', [])  # Updated key name
+#         for node in nodes:
+#             # Extract field names from the 'fields' list
+#             fields = [field['name'] for field in node['fields']]
+#             # Add nodes to the graph
+#             label = '{' + node['name'] + '|' + '|'.join(fields) + '}'
+#             G.add_node(node['name'], shape='record', label=label)
+#         for relation in relations:  # Updated variable name
+#             # Add edges to the graph
+#             G.add_edge(relation['from'], relation['to'], label=relation['relation'])
+
+# # Render the graph to a file
+# G.draw('assets/ssd_erd_multiyaml.png', prog='dot', format='png')
+
+
+# # including the new 'return' group info
 import glob
 import yaml
 import pygraphviz as pgv
@@ -13,10 +42,16 @@ for file_path in glob.glob('structure_split_objects/*.yml'):
         nodes = data.get('nodes', [])
         relations = data.get('relations', [])  # Updated key name
         for node in nodes:
-            # Extract field names from the 'fields' list
-            fields = [field['name'] for field in node['fields']]
+            # Extract field names and group labels from the 'fields' list
+            field_labels = []
+            for field in node['fields']:
+                field_name = field['name']
+                if 'group' in field:
+                    group_labels = ', '.join(field['group'])
+                    field_name = f"{field_name} [{group_labels}]"
+                field_labels.append(field_name)
             # Add nodes to the graph
-            label = '{' + node['name'] + '|' + '|'.join(fields) + '}'
+            label = '{' + node['name'] + '|' + '|'.join(field_labels) + '}'
             G.add_node(node['name'], shape='record', label=label)
         for relation in relations:  # Updated variable name
             # Add edges to the graph
@@ -24,35 +59,3 @@ for file_path in glob.glob('structure_split_objects/*.yml'):
 
 # Render the graph to a file
 G.draw('assets/ssd_erd_multiyaml.png', prog='dot', format='png')
-
-
-# # including the new 'return' group info
-# import glob
-# import yaml
-# import pydot
-
-# # Create a new directed graph
-# G = pydot.Dot(graph_type='digraph')
-
-# # Load and combine YAML files
-# for file_path in glob.glob('structure_split_objects/*.yml'):
-#     with open(file_path) as f:
-#         data = yaml.safe_load(f)
-#         if 'fields' in data:
-#             node_name = data['name']
-#             fields = data['fields']
-#             field_labels = [field['name'] + (' [' + ', '.join(field.get('group', [])) + ']' if 'group' in field else '') for field in fields]
-#             # Create the label string for the entity/object
-#             label = '{' + node_name + '|' + '|'.join(field_labels) + '}'
-#             # Add node with custom shape and label
-#             node = pydot.Node(node_name, shape='record', label=label)
-#             G.add_node(node)
-#         elif 'edges' in data:
-#             edges = data['edges']
-#             for edge in edges:
-#                 G.add_edge(pydot.Edge(edge['from'], edge['to'], label=edge['relation']))
-
-# # Render the graph to a file
-# G.write_svg('assets/ssd_erd_multiyaml.svg')
-
-
