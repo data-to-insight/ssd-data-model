77eb7bbcca66b7409c10447fea73a54400594e98
Rob Harrison <48765695+robjharrison@users.noreply.github.com>
2023-06-19 16:09:56 +0000

    Baseline included fixed list field fixes

--- create_erd_from_yaml.py
+++ create_erd_from_yaml.py
@@ -1,30 +1,3 @@
-# import glob
-# import yaml
-# import pygraphviz as pgv
-
-
-# # Create a new directed graph
-# G = pgv.AGraph(directed=True)
-
-# # Load and combine YAML files
-# for file_path in glob.glob('structure_split_objects/*.yml'):
-#     with open(file_path) as f:
-#         data = yaml.safe_load(f)
-#         nodes = data.get('nodes', [])
-#         relations = data.get('relations', [])  # Updated key name
-#         for node in nodes:
-#             # Extract field names from the 'fields' list
-#             fields = [field['name'] for field in node['fields']]
-#             # Add nodes to the graph
-#             label = '{' + node['name'] + '|' + '|'.join(fields) + '}'
-#             G.add_node(node['name'], shape='record', label=label)
-#         for relation in relations:  # Updated variable name
-#             # Add edges to the graph
-#             G.add_edge(relation['from'], relation['to'], label=relation['relation'])
-
-# # Render the graph to a file
-# G.draw('assets/ssd_erd_multiyaml.png', prog='dot', format='png')
-
 
 # # including the new 'return' group info
 import glob
@@ -36,7 +9,7 @@ import pygraphviz as pgv
 G = pgv.AGraph(directed=True)
 
 # Load and combine YAML files
-for file_path in glob.glob('structure_split_objects/*.yml'):
+for file_path in glob.glob('data/objects/*.yml'):
 
     with open(file_path) as f:
         data = yaml.safe_load(f)
@@ -59,4 +32,4 @@ for file_path in glob.glob('structure_split_objects/*.yml'):
             G.add_edge(relation['from'], relation['to'], label=relation['relation'])
 
 # Render the graph to a file
-G.draw('assets/ssd_erd_multiyaml_testing.png', prog='dot', format='png')
+G.draw('assets/ssd_erd_multiyaml.png', prog='dot', format='png')
