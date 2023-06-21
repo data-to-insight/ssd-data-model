5bd025f75250e283a85cd96d8de1d4f44269d90f
Rob Harrison <48765695+robjharrison@users.noreply.github.com>
2023-06-21 10:00:14 +0000

    html generation for object defs added

--- generate_html.py
+++ generate_html.py
@@ -1,68 +1,47 @@
 import glob
 import yaml
 import base64
-import pygraphviz as pgv
-from PIL import Image
+import subprocess
 
 # Generate HTML content
-html_content = "<html>\n<head>\n<title>SSD Data Model</title>\n</head>\n<body>\n"
+html_content = "<html><head><style>"
+html_content += "table { border-collapse: collapse; width: 80%; margin: auto; }"
+html_content += "th, td { text-align: left; padding: 8px; }"
+html_content += "th { background-color: #f2f2f2; }"
+html_content += "</style></head><body>"
+html_content += "<h1>SSD Data Model Documentation</h1>"
 
-# Iterate over each entity definition
-for entity_file in glob.glob("data/objects/*.yml"):
-    with open(entity_file, 'r') as f:
-        entity = yaml.safe_load(f)
-        entity_name = entity['name']
-        html_content += f"<h2>{entity_name}</h2>\n"
-
-        # Create a new directed graph
-        G = pgv.AGraph(directed=True)
-
-        # Add the entity node to the graph
-        label = f"{{<b>{entity_name}</b>}}"
-        G.add_node(entity_name, shape='plaintext', label=label)
-
-        # Check for fields in the entity definition
-        if 'fields' in entity:
-            for field in entity['fields']:
+# Read the YAML files
+for file_path in glob.glob('data/objects/*.yml'):
+    with open(file_path) as f:
+        data = yaml.safe_load(f)
+        nodes = data.get('nodes', [])
+        if nodes:
+            entity_name = nodes[0]['name']
+            html_content += f'<h2>{entity_name}</h2>'
+            html_content += "<table>"
+            html_content += "<tr><th>Field</th><th>Constraints</th><th>CMS</th><th>Categories</th></tr>"
+            for field in nodes[0]['fields']:
                 field_name = field['name']
                 constraints = field.get('constraints', '')
                 cms = field.get('cms', '')
                 categories = field.get('categories', '')
+                html_content += f"<tr><td>{field_name}</td><td>{constraints}</td><td>{cms}</td><td>{categories}</td></tr>"
+            html_content += "</table>"
 
-                # Create the field label
-                label = f"<b>{field_name}</b><br/>Constraints: {constraints}<br/>CMS: {cms}<br/>Categories: {categories}"
+# Add ERD image to the HTML content
+with open('/workspaces/ssd-data-model/assets/ssd_erd_yml.png', 'rb') as f:
+    erd_image_data = f.read()
+erd_image_base64 = base64.b64encode(erd_image_data).decode('utf-8')
+html_content += f'<div style="text-align: center; margin-top: 50px;">'
+html_content += f'<img src="data:image/png;base64,{erd_image_base64}" alt="SSD ERD Diagram">'
+html_content += "</div>"
 
-                # Add the field node to the graph
-                G.add_node(field_name, shape='plaintext', label=label)
+html_content += "</body></html>"
 
-                # Add an edge between the entity and the field
-                G.add_edge(entity_name, field_name)
-
-        # Render the graph to a file
-        erd_file = f"assets/{entity_name}_erd.png"
-        G.draw(erd_file, prog='dot', format='png')
-
-        # Resize the image to fit the browser window
-        img = Image.open(erd_file)
-        img.thumbnail((1280, 1280))  # Set the maximum size
-        img.save(erd_file)
-
-        # Encode the image as base64
-        with open(erd_file, "rb") as image_file:
-            erd_image_data = image_file.read()
-        erd_image_base64 = base64.b64encode(erd_image_data).decode("utf-8")
-
-        # Add the image to the HTML content
-        html_content += f'<img src="data:image/png;base64,{erd_image_base64}">\n'
-
-# Finish generating the HTML content
-html_content += "</body>\n</html>"
-
-# Write the HTML content to a file
 # Write HTML content to file
-with open('docs/index.html', 'w') as f:
+with open('/workspaces/ssd-data-model/docs/index.html', 'w') as f:
     f.write(html_content)
 
-
-
-print("HTML file generated successfully.")
+# Run create_erd_from_yml.py script
+subprocess.run(['python3', '/workspaces/ssd-data-model/create_erd_from_yml.py'])
