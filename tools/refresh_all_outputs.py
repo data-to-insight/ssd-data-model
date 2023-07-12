351958eb8e8891ec04f545cd0f7707ff867e857b
Rob Harrison <48765695+robjharrison@users.noreply.github.com>
2023-07-12 15:06:05 +0000

    changelog refresh script added

--- tools/refresh_all_outputs.py
+++ tools/refresh_all_outputs.py
@@ -6,8 +6,12 @@ paths = get_paths()
 
 # creating base objects and essentials
 
+# Run script to refresh changelog in case workflow action run not recent
+subprocess.run(['python3', paths['tools'] + 'update_changelog.py'])
+
 # Run script to re-create the individual object diagram images
 subprocess.run(['python3', paths['tools'] + '1_generate_yaml_objects.py'])
+
 
 
 # diagrams and visuals
@@ -26,4 +30,7 @@ subprocess.run(['python3', paths['tools'] + '3_generate_html_conceptual.py'])
 
 # Run script to re-create the individual object diagram images
 subprocess.run(['python3', paths['tools'] + '3_generate_html_guidance.py'])
+
+# Run script to re-create the individual object diagram images
+subprocess.run(['python3', paths['tools'] + '3_generate_html_datamapping.py'])
 
