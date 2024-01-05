87087f2e4e2e10086331db6208b4c064aebd6a05
Rob Harrison <48765695+robjharrison@users.noreply.github.com>
2024-01-05 15:27:25 +0000

    AA Ofsted List 1 - revised obj/item fixes

--- tools/cms_extract/AnnexA.sql
+++ tools/cms_extract/AnnexA.sql
@@ -11,12 +11,13 @@ Description: List 1: Contacts "All contacts received in the six months before th
 Where a contact refers to multiple children, include an entry for each child in the contact.
 
 Author: D2I
-Last Modified Date: 
+Last Modified Date: 05/01/32 RH
 DB Compatibility: SQL Server 2014+|...
-Version: 0.1
-Status: [*Dev, Testing, Release, Blocked, AwaitingReview, Backlog]
-Remarks: script needs modifying to ref revised field/item naming 271023
+Version: 0.2
+Status: [Dev, *Testing, Release, Blocked, AwaitingReview, Backlog]
+Remarks: script modified to reflect new|revised obj/item naming. 
 Dependencies: 
+- @YearsBack
 - ssd_contacts
 - ssd_person
 =============================================================================
@@ -26,37 +27,37 @@ Dependencies:
 CREATE VIEW AA_1_contacts_vw AS
 SELECT
     /* Common AA fields */
-    p.la_person_id,
-    p.person_gender,
-    p.person_ethnicity,
-    FORMAT(p.person_dob, 'dd/MM/yyyy') AS formatted_person_dob,
+    p.pers_person_id,
+    p.pers_sex,
+    p.pers_ethnicity,
+    FORMAT(p.pers_dob, 'dd/MM/yyyy') AS formatted_pers_dob,
     CASE -- provide the child's age in years at their last birthday.
-        WHEN p.person_dob > GETDATE() THEN -1 -- If a child is unborn, enter their age as '-1'
+        WHEN p.pers_dob > GETDATE() THEN -1 -- If a child is unborn, enter their age as '-1'
         -- If born on Feb 29 and the current year is not a leap year and the date is before Feb 28, adjust the age
-        WHEN MONTH(p.person_dob) = 2 AND DAY(p.person_dob) = 29 AND
+        WHEN MONTH(p.pers_dob) = 2 AND DAY(p.pers_dob) = 29 AND
             MONTH(GETDATE()) <= 2 AND DAY(GETDATE()) < 28 AND
             (YEAR(GETDATE()) % 4 != 0 OR (YEAR(GETDATE()) % 100 = 0 AND YEAR(GETDATE()) % 400 != 0))
-        THEN YEAR(GETDATE()) - YEAR(p.person_dob) - 2
+        THEN YEAR(GETDATE()) - YEAR(p.pers_dob) - 2
         ELSE 
-            YEAR(GETDATE()) - YEAR(p.person_dob) - 
+            YEAR(GETDATE()) - YEAR(p.pers_dob) - 
             CASE 
-                WHEN MONTH(GETDATE()) < MONTH(p.person_dob) OR 
-                    (MONTH(GETDATE()) = MONTH(p.person_dob) AND DAY(GETDATE()) < DAY(p.person_dob))
+                WHEN MONTH(GETDATE()) < MONTH(p.pers_dob) OR 
+                    (MONTH(GETDATE()) = MONTH(p.pers_dob) AND DAY(GETDATE()) < DAY(p.pers_dob))
                 THEN 1 
                 ELSE 0 -- returned if age is < 1yr
             END
     END as CurrentAge, -- Calculated Age (Note on List 1 is 'AGE')
     
     /* Returns fields */
-    FORMAT(c.contact_date, 'dd/MM/yyyy') AS formatted_contact_date,
-    c.contact_source
+    FORMAT(c.cont_contact_date, 'dd/MM/yyyy') AS formatted_cont_contact_date,
+    c.cont_contact_source
 
 FROM
-    contacts c
+    ssd_contacts c
 LEFT JOIN
-    person p ON c.la_person_id = p.la_person_id
+    ssd_person p ON c.cont_person_id = p.pers_person_id
 WHERE
-    c.contact_date >= DATEADD(MONTH, -@YearsBack, GETDATE());
+    c.cont_contact_date >= DATEADD(MONTH, -@YearsBack, GETDATE());
 
 
 
