087787f8385d6a96315cfa67dcd0ae42b2a3ed69
Rob Harrison <48765695+robjharrison@users.noreply.github.com>
2024-09-02 13:59:33 +0000

    Set up east-sussex with bespoke files

--- cms_ssd_extract_sql/mosaic/live/ssd_assessment_factors_tag_dw.sql
+++
@@ -1,5 +0,0 @@
-select
-	fact.ASSESSMENT_ID cinf_assessment_id,
-	fact.FACTOR_VALUE cinf_assessment_factors_json
-from
-	dm_cin_assess_factors fact
--- cms_ssd_extract_sql/mosaic/live/ssd_disability_essex.sql
+++
@@ -1,6 +0,0 @@
-select
-	dis.PERSON_ID disa_person_id,
-	null disa_table_id,
-	dis.CIN_DISABILITY_CATEGORY_CODE disa_disability_code
-from
-	SCF.Disabilities dis
