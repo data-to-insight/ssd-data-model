957fcedf55d02f999f939cbfc0d3187cd650044b
Rob Harrison <48765695+robjharrison@users.noreply.github.com>
2026-03-23 16:43:56 +0000

    ssd_cla_convictions.clac_cla_conviction_offence missing field added

--- deployment_extracts/mosaic/live/ssd_deployment_individual_files/ssd_cla_convictions_tag_dw.sql
+++ deployment_extracts/mosaic/live/ssd_deployment_individual_files/ssd_cla_convictions_tag_dw.sql
@@ -1,7 +1,9 @@
 select
 	o.offence_id clac_cla_conviction_id
 	,o.person_id clac_person_id
-	,cast(o.offence_date as date) clac_cla_conviction_date
+    CAST(o.offence_date AS date)     AS clac_cla_conviction_date,
+    CAST(NULL AS VARCHAR(1000))      AS clac_cla_conviction_offence
+
 from 
 	dm_offences o
 where 
